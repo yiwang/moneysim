0edd1aabdac6b845d1a03c5a1bf38dd6a1857599
wangyuan327 <wangyuan327@70138a7c-f2df-11dd-984d-ab748d24aa7e>
2009-02-13 22:41:06 +0000

    2 new files

---
+++ src/Money Game.py
@@ -0,0 +1,90 @@
+#! C:\Python25\python.exe
+# -*- coding: utf-8 -*-
+
+import random
+
+
+
+B=100
+rr=0.1
+N1=5
+N2=5
+cr=0.1
+C=50
+D=500
+Consumer_C = [10,10,10,10,10]
+Consumer_D = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+Consumer_L = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+Bank_C = [50,50,50,50,50]
+Bank_L = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+Bank_R = [0,0,0,0,0]
+
+
+i=0
+c=0
+b=0
+
+while (i<5):
+    deposit_state = random.randrange(0,2)
+    loan_state = random.randrange(0,2)
+    if (Consumer_C[i]>0) and (deposit_state == 1):
+        Consumer_D[i][i] = Consumer_D[i][i] + 0.5 * Consumer_C[i] 
+    if (Consumer_C[i]>0) and (loan_state == 1):
+        Consumer_L[i][i] = Consumer_L[i][i] + 0.25 * Consumer_C[i]
+    Consumer_C[i] = Consumer_C[i] + Consumer_L[i][i] - Consumer_D[i][i]
+    i=i+1
+i=0
+print "The Consumer's Currency is"
+while (c<5):
+    print "%0.2f" % (float(Consumer_C[c])),
+    c=c+1
+c=0
+print " "
+print "The Consumer's Deposit is"
+while (c<5):
+    print "%0.2f" % (float(Consumer_D[c][c])),
+    c=c+1
+c=0
+print " "
+print "The Consumer's Loan is"
+while (c<5):
+    print "%0.2f" % (float(Consumer_L[c][c])),
+    c=c+1
+c=0
+while(b<5):
+    Bank_C[b]= Bank_C[b] * (1-rr)
+    Bank_R[b] = Bank_C[b] * rr
+    bank_loan_state = random.randrange(0,2)
+    if (b!=0) and (b != 4) and (Bank_C[b+1]>0) and (bank_loan_state == 1):
+        Bank_L[b][b+1] = 0.1 * Bank_C[b+1] + Bank_L[b][b+1]
+    if(Bank_C[0]>0) and (bank_loan_state == 1) and (b == 4):
+        Bank_L[4][0] = 0.1 * Bank_C[0] + Bank_L [4][0]
+    if (b==0) and (Bank_C[1]>0) and (bank_loan_state == 1):
+        Bank_L[0][1] = 0.1 * Bank_C[1] + Bank_L[0][1]
+    b=b+1
+b=0
+while(b<5):
+    if(b==0):
+        Bank_C[0] = Bank_C[0] + Bank_L[0][1]+ Consumer_D[0][0] - Consumer_L[0][0]-Bank_L[4][0]
+    elif(b==4):
+        Bank_C[4] = Bank_C[4] + Bank_L[4][0]+ Consumer_D[4][4] - Consumer_L[4][4]-Bank_L[3][4]
+    else :
+        Bank_C[b] = Bank_C[b] + Bank_L[b][b+1]+ Consumer_D[b][b] - Consumer_L[b][b]-Bank_L[b-1][b]
+    b=b+1
+b=0
+print ""
+print "The Bank's Loan is "
+while(c<5):
+    if(c<4):
+        print "%0.2f" % (float(Bank_L[c][c+1])),
+    if(c==4):
+        print "%0.2f" % (float(Bank_L[4][0]))
+    c=c+1
+c=0
+print "The Bank's Currency is "
+while(c<5):
+    print "%0.2f" % (float(Bank_C[c])),
+    c=c+1
+c=0
+print ""
+
